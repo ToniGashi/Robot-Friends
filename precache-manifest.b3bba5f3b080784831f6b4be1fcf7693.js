61b8478162e909048f370db01759c85145f49956
ToniGashi <tonigashi999@gmail.com>
2020-10-26 20:00:40 +0200

    Updates

---
+++ precache-manifest.b3bba5f3b080784831f6b4be1fcf7693.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caab6cbf610e16b7b5d0c3cdf242abd8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5851779c3e0d0c66b43c",
+    "url": "/robofriends/static/css/main.25e0a683.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5851779c3e0d0c66b43c",
+    "url": "/robofriends/static/js/main.cf5acf96.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "739de675571e932bad82b3d480174f2b",
+    "url": "/robofriends/static/media/New Walt Disney UI.739de675.woff"
+  },
+  {
+    "revision": "3e9766e84a7eef37f5fb68f7a2a03454",
+    "url": "/robofriends/static/media/kingthings-willow.regular.3e9766e8.woff"
+  }
+]);
